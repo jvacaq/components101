d9ea2a78a86996270c4ce9d962405f17b607dbc4
Juan Carlos Vaca <jvaca@advlatam.com>
2022-06-01 16:24:57 -0500

    Plantillas e interpolaciones
    *ngIf *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'components101';
+  myObject = {
+    gender: 'male',
+    age: 33,
+    location: 'EC'
+  };
+  myArr = ['him', 'hers', 'yours'];
+  myBoolean = true;
 }
